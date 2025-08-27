e0266ff8f7f36ce8cd989f3e68d085bf42c6aae3
Jatesh Parikh <jateshparikh97@gmail.com>
2025-08-27 16:27:55 +0530

    hero slice added

--- src/hooks/usePrefersReducedMotion.js
+++ src/hooks/usePrefersReducedMotion.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 const QUERY = "(prefers-reduced-motion: no-preference)";
 
 const isRenderingOnServer = typeof window === undefined;
--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,6 +1,15 @@
+import Bounded from "@/components/Bounded";
+import ButtonLink from "@/components/ButtonLink";
+import StarGrid from "@/components/StarGrid";
+import { Content, isFilled } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import {
+  PrismicRichText,
+  PrismicText,
+  SliceComponentProps,
+} from "@prismicio/react";
+import AnimatedContent from "./AnimatedContent";
 import { FC } from "react";
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
 
 /**
  * Props for `Hero`.
@@ -12,40 +21,13 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero: FC<HeroProps> = ({ slice }) => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="text-center"
     >
-      Placeholder component for hero (variation: {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server@latest"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select a model optimized for coding (e.g. Claude 3.7 Sonnet or similar)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 🎙️ Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       * 📚 Documentation: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
-    </section>
+      <AnimatedContent slice={slice} />
+    </Bounded>
   );
 };
 
