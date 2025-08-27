4d09e549b2e564b490f8327260bab8b1a5f8b096
Jatesh Parikh <jateshparikh97@gmail.com>
2025-08-27 18:50:04 +0530

    showcase slice added

--- src/slices/Showcase/index.tsx
+++ src/slices/Showcase/index.tsx
@@ -1,6 +1,17 @@
-import { FC } from "react";
+import Bounded from "@/components/Bounded";
+import ButtonLink from "@/components/ButtonLink";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { FC } from "react";
+import clsx from "clsx";
+import { PiArrowsClockwise, PiGear } from "react-icons/pi";
+import AnimatedContent from "./AnimatedContent";
+
+const icons = {
+  gear: <PiGear />,
+  cycle: <PiArrowsClockwise />,
+};
 
 /**
  * Props for `Showcase`.
@@ -12,40 +23,59 @@ export type ShowcaseProps = SliceComponentProps<Content.ShowcaseSlice>;
  */
 const Showcase: FC<ShowcaseProps> = ({ slice }) => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="relative"
     >
-      Placeholder component for showcase (variation: {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server@latest"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select a model optimized for coding (e.g. Claude 3.7 Sonnet or similar)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 🎙️ Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       * 📚 Documentation: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
-    </section>
+      <div className="glow absolute -z-10 aspect-square w-full max-w-xl rounded-full bg-blue-400/20 blur-3xl filter" />
+
+      <AnimatedContent>
+        <PrismicRichText
+          field={slice.primary.heading}
+          components={{
+            heading2: ({ children }) => {
+              return (
+                <h2 className="md: text-center text-5xl font-medium text-balance md:text-7xl">
+                  {children}
+                </h2>
+              );
+            },
+          }}
+        />
+      </AnimatedContent>
+
+      <div className="mt-16 grid items-center gap-8 rounded-xl border border-blue-50/20 bg-gradient-to-b from-slate-50/15 to-slate-50/5 p-8 backdrop-blur-sm lg:grid-cols-3 lg:gap-0 lg:py-12">
+        <div>
+          <div className="w-fit rounded-lg bg-blue-500/35 p-4 text-3xl">
+            <>{slice.primary.icon && icons[slice.primary.icon]}</>
+          </div>
+
+          <div className="mt-6 text-2xl font-normal">
+            <PrismicRichText field={slice.primary.sub_heading} />
+          </div>
+
+          <div className="prose prose-invert mt-4 max-w-xl">
+            <PrismicRichText field={slice.primary.body} />
+          </div>
+
+          <ButtonLink field={slice.primary.button_link} className="mt-6">
+            {slice.primary.button_text || "Learn More"}
+          </ButtonLink>
+        </div>
+
+        <PrismicNextImage
+          field={slice.primary.image}
+          className={clsx(
+            "opacity-90 shadow-2xl lg:col-span-2 lg:pt-0",
+            slice.variation === "reverse"
+              ? "lg:order-1 lg:translate-x-[15%]"
+              : "lg:-order-1 lg:translate-x-[-15%]",
+          )}
+          sizes="(max-width: 768px) 100vw, 50vw"
+        />
+      </div>
+    </Bounded>
   );
 };
 
