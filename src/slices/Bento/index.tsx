8323764963960838c8ca030147d54c542926be0f
Jatesh Parikh <jateshparikh97@gmail.com>
2025-08-27 15:12:23 +0530

    bento slice added

--- src/slices/Bento/index.tsx
+++ src/slices/Bento/index.tsx
@@ -1,6 +1,13 @@
-import { FC } from "react";
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import Bounded from "@/components/Bounded";
+import { FC, JSX } from "react";
+import { Content, asText } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import {
+  PrismicRichText,
+  PrismicText,
+  SliceComponentProps,
+} from "@prismicio/react";
+import clsx from "clsx";
 
 /**
  * Props for `Bento`.
@@ -10,42 +17,58 @@ export type BentoProps = SliceComponentProps<Content.BentoSlice>;
 /**
  * Component for "Bento" Slices.
  */
-const Bento: FC<BentoProps> = ({ slice }) => {
+const Bento = ({ slice }: BentoProps) => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for bento (variation: {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server@latest"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select a model optimized for coding (e.g. Claude 3.7 Sonnet or similar)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 🎙️ Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       * 📚 Documentation: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
-    </section>
+      <PrismicRichText
+        field={slice.primary.heading}
+        components={{
+          heading2: ({ children }) => (
+            <h2 className="md:text7xl text-center text-5xl font-medium text-balance">
+              {children}
+            </h2>
+          ),
+          em: ({ children }) => (
+            <em className="bg-gradient-to-b from-yellow-100 to-yellow-500 bg-clip-text text-transparent not-italic">
+              {children}
+            </em>
+          ),
+        }}
+      />
+
+      <div className="mx-auto mt-6 max-w-md text-center text-balance text-slate-300">
+        <PrismicRichText field={slice.primary.body} />
+      </div>
+
+      <div className="mt-16 grid max-w-4xl grid-rows-[auto_auto_auto] gap-8 md:grid-cols-3 md:gap-10">
+        {slice.primary.bento_grid.map((item) => {
+          return (
+            <div
+              className={clsx(
+                "glass-container row-span-3 grid grid-rows-subgrid gap-4 rounded-lg bg-gradient-to-b from-gray-900 to-gray-950 p-4",
+                item.wide ? "md:col-span-2" : "md:col-span-1",
+              )}
+              key={asText(item.title)}
+            >
+              <h3 className="text-2xl">
+                <PrismicText field={item.title} />
+              </h3>
+
+              <div className="max-w-md text-balance text-slate-300">
+                <PrismicRichText field={item.body} />
+              </div>
+              <PrismicNextImage
+                field={item.image}
+                className="max-h-36 w-auto"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </Bounded>
   );
 };
 
