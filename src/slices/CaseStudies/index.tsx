448ddafc41cd9953da06154eb32516556e2ddc47
Jatesh Parikh <jateshparikh97@gmail.com>
2025-08-27 15:43:15 +0530

    case-studies slice added

--- src/slices/CaseStudies/index.tsx
+++ src/slices/CaseStudies/index.tsx
@@ -1,6 +1,14 @@
+import Bounded from "@/components/Bounded";
+import { createClient } from "@/prismicio";
+import { Content, isFilled } from "@prismicio/client";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { FC } from "react";
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import {
+  PrismicRichText,
+  PrismicText,
+  SliceComponentProps,
+} from "@prismicio/react";
+import clsx from "clsx";
 
 /**
  * Props for `CaseStudies`.
@@ -10,43 +18,71 @@ export type CaseStudiesProps = SliceComponentProps<Content.CaseStudiesSlice>;
 /**
  * Component for "CaseStudies" Slices.
  */
-const CaseStudies: FC<CaseStudiesProps> = ({ slice }) => {
+const CaseStudies: FC<CaseStudiesProps> = async ({ slice }) => {
+  const client = createClient();
+
+  const caseStudies = await Promise.all(
+    slice.primary.case_studies.map(async (item) => {
+      if (isFilled.contentRelationship(item.case_study)) {
+        return await client.getByID<Content.CaseStudyDocument>(
+          item.case_study.id,
+        );
+      }
+    }),
+  );
+
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for case_studies (variation: {slice.variation})
-      slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server@latest"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select a model optimized for coding (e.g. Claude 3.7 Sonnet or similar)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 🎙️ Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       * 📚 Documentation: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
-    </section>
+      <h2 className="max-2xl text-center text-5xl font-medium text-balance md:text-7xl">
+        <PrismicText field={slice.primary.heading} />
+      </h2>
+
+      <div className="mx-auto mt-6 max-w-md text-center text-balance text-slate-300">
+        <PrismicRichText field={slice.primary.body} />
+      </div>
+
+      <div className="mt-20 grid gap-16">
+        {caseStudies.map(
+          (caseStudy, index) =>
+            caseStudy && (
+              <div
+                key={caseStudy.id}
+                className="relative grid gap-4 opacity-85 transition-opacity duration-300 hover:cursor-pointer hover:opacity-100 md:grid-cols-2 md:gap-8 lg:grid-cols-3"
+              >
+                <div className="col-span-1 flex flex-col justify-center gap-4">
+                  <h3 className="text-4xl">
+                    <PrismicText field={caseStudy.data.company} />
+                  </h3>
+
+                  <div className="max-w-md">
+                    <PrismicRichText field={caseStudy.data.description} />
+                  </div>
+
+                  <PrismicNextLink
+                    document={caseStudy}
+                    className="after:absolute after:inset-0 hover:underline"
+                  >
+                    Read <PrismicText field={caseStudy.data.company} /> case
+                    study
+                  </PrismicNextLink>
+                </div>
+                <PrismicNextImage
+                  field={caseStudy.data.logo_image}
+                  quality={100}
+                  sizes="(max-width: 768px) 100vw, 50vw"
+                  className={clsx(
+                    "rounded-xl lg:col-span-2",
+                    index % 2 && "md:-order-1",
+                  )}
+                />
+              </div>
+            ),
+        )}
+      </div>
+    </Bounded>
   );
 };
 
