30425865c198951b7ea6abbd648f91ce4d050fcc
Jatesh Parikh <jateshparikh97@gmail.com>
2025-08-27 19:30:57 +0530

    modify button-link component for prod

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -6,7 +6,7 @@ export default function ButtonLink({
   ...restProps
 }: PrismicNextLinkProps) {
   return (
-    // @ts-expect-error
+    // @ts-expect-error some issue with Next.js and Prismic types
     <PrismicNextLink
       className={clsx(
         "relative inline-flex h-fit w-fit rounded-full border border-blue-100/20 bg-blue-200/10 px-4 py-2 text-blue-200 ring-yellow-300 transition-colors outline-none after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-black/0 after:blur-md after:transition-all after:duration-500 hover:border-yellow-200/40 hover:text-yellow-300 after:hover:bg-black/15 focus:ring-2",
